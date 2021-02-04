98935c059ffc04abf15a7d73f8e5f6625eb471ce
peaceSh4wn <showpig22@126.com>
2021-02-04 23:46:32 +0800

    socket server structure

--- c2/M4ster/src/master.c
+++ c2/M4ster/src/master.c
@@ -1,1 +1,57 @@
 #include "master.h"
+#include "usage.h"
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	if (SUCCESS != init_server()) {
+		printf("init server error\n");
+		goto err;
+	}
+	
+	if (SUCCESS != loop()) {
+		printf("loop error\n");	
+		goto err;
+	}
+
+	if (SUCCESS != close_server()) {
+		printf("close server error\n");
+		goto err;
+	}
+	
+	return 0;
+err:
+	printf("fatal error, quitting...\n");
+	return -1;
+}
+
+int init_server()
+{
+	Gst = (SockTag *)calloc(1, sizeof(SockTag));			
+
+	Gst->sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (-1 == Gst->sock_fd) {
+		// failed	
+		goto err;
+	}
+
+	memset(&(Gst->sk_addr), 0, sizeof(sockaddr_in));
+	Gst->sk_addr->sin_family = AF_IPV4;
+	Gst->sk_addr->sin_
+
+	
+	return SUCCESS;
+err:
+	// XFREE();
+	return FAILURE;
+}
+
+void loop()
+{
+
+}
+
+int close_server()
+{
+
+}
--- c2/M4ster/src/master.h
+++ c2/M4ster/src/master.h
@@ -2,7 +2,47 @@
 #define _MASTER_H_
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define DATA_LEN 2048
+
+// get local ip automatically in next version
+#define LOCAL_IP "192.168.204.146"
+
+typedef enum {
+	FAILURE = -1,
+	SUCCESS
+} STAT;
+
+typedef struct _sockTag {
+	int sock_fd;
+	struct sockaddr_in sk_addr;
+}SockTag;
+
+typdef struct _sockData {
+	char readData[DATA_LEN];
+	char sendData[DATA_LEN];
+	Sock_Tag sock_t;
+}SockData;
+
+/* global socket desciptor */
+SockTag *Gst = NULL;
+
+/* global socket data */
+SockData *Gsd = NULL;
+
+/* initilize global socket and prepare 
+ * tcp connection. we'll use multithreading
+ * in future */
+int init_server();
+
+/* get data, process instruction then send data */
+void loop();
+
+/* close server */
+int close_server();
 
 #endif
